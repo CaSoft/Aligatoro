a097a69490e53d966b3c252d6d2beaeb8a9849f9
InFog <junior@casoft.info>
2011-02-02 16:23:55 -0200

    Definida a configuração básica do CodeIgniter e adicionadas informações ao diagrama de classes.

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'aligatoro';
+$db['default']['password'] = 'aligatoro';
+$db['default']['database'] = 'aligatoro';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
