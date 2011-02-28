7285bed2f061c38c9df6d584f000c51e936ec4dd
InFog <junior@casoft.info>
2011-02-28 11:46:34 -0300

    Passando a página de login pelo validador do W3C. Tudo OK.

--- application/views/login.php
+++ application/views/login.php
@@ -16,13 +16,15 @@
     <body>
         <div id="caixa">
             <form method="post" action="<?php echo site_url(); ?>login">
-                <label for="login">
-                    Login <input type="text" name="login" id="login" value="" maxlength="10" />
-                </label>
-                <label for="senha">
-                    Senha <input type="password" name="senha" id="senha" value="" />
-                </label>
-                <input type="submit" value="Autenticar" />
+                <fieldset>
+                    <label for="login">
+                        Login <input type="text" name="login" id="login" value="" maxlength="10" />
+                    </label>
+                    <label for="senha">
+                        Senha <input type="password" name="senha" id="senha" value="" />
+                    </label>
+                    <input type="submit" value="Autenticar" />
+                </fieldset>
             </form>
         </div><!-- caixa -->
         <?php if ($falha_login) : ?>
