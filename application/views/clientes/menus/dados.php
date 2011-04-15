84c87bba2d9480086abf4a606e149985756b7a0b
InFog <junior@casoft.info>
2011-04-15 11:43:46 -0300

    Alterado o botão de impressão dos dados dos clientes (issue #2);

--- application/views/clientes/menus/dados.php
+++ application/views/clientes/menus/dados.php
@@ -1,7 +1,7 @@
 <h2>Opções</h2>
 <ul>
     <li>
-        <a href="<?php echo site_url(); ?>clientes/impressao/<?php echo $cliente['id']; ?>" title="Imprimir os dados do cliente" class="impressao_popup">Imprimir cadastro</a>
+        <a href="<?php echo site_url(); ?>clientes/impressao/<?php echo $cliente['id']; ?>" title="Imprimir os dados do cliente" class="impressao_popup">Imprimir dados</a>
     </li>
     <li>
         <a href="<?php echo site_url(); ?>clientes/editar/<?php echo $cliente['id']; ?>" title="Editar o cadastro">Editar</a>
